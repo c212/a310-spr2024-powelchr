d2c5f69e183140187279f48264ea2c325fd13e25
christian-powe11 <christianpowell01@gmail.com>
2024-01-18 16:41:07 -0500

    lab2

--- labs/lab02.py
+++ labs/lab02.py
@@ -93,3 +93,107 @@ print(a.talk())
 
 a = dog('daisy')
 print(a.talk())
+
+import math 
+
+class point:
+    def __init__(self, x,y) -> None:
+        self.x = x
+        self.y = y
+
+    def __str__(self) -> str:
+        return '(' + str(self.x) + ', ' + str(self.y) + ')'
+    
+    def distanceTo(self, second):
+        dx = self.x - second.x
+        dy = self.y - second.y 
+        return math.sqrt(dx**2 + dy**2)
+    
+a = point(3,2)
+b = point(-1,5)
+print(a)
+print(b)
+print(a.distanceTo(b))
+
+class line:
+    def __init__(self, p1, p2) -> None:
+        self.p1 = p1 
+        self.p2 = p2 
+
+    def __str__(self) -> str:
+        return 'Line: (' + str(self.x) + ', ' + str(self.y) + ')'
+    
+    def length(self):
+        dist = self.p1.distanceTo(self.p2)
+        return dist 
+
+class triangle:
+    def __init__(self, p1, p2, p3) -> None:
+        self.l1 = line(p1, p2)
+        self.l2 = line(p2, p3)
+        self.l3 = line(p3, p1)
+
+    def __str__ (self) -> str:
+        return "triangle {" + str(self.l1.p1) + ", " +  str(self.l2.p1) + ", " + str(self.l3.p1) + "}"
+
+    def area(self):
+        # A = √s(s−a)(s−b)(s−c)
+        a = self.l1.length()
+        b = self.l2.length()
+        c = self.l3.length()
+        s = (a + b + c)/2
+
+        return math.sqrt(s*(s-a)*(s-b)*(s-c))
+
+
+a = point(4,5)
+b = point(2,2)
+c = point(5,1)
+tri = triangle(a,b,c)
+A = tri.area()
+print("Area of ", tri, " is: ", A)
+
+class LinkedList:
+    def __init__(self, num, next) -> None:
+        self.num = num 
+        self.next = next 
+
+    def show(self):
+        temp = self 
+        result = ''
+        while (temp):
+            result = result + '--> ' + temp.num + '--> '
+            temp = temp.next 
+        print(result)
+
+a = LinkedList('christa', None)
+a = LinkedList('ahmed', a)
+a.show()
+
+class binarytree:
+    def __init__(self, value, right, left) -> None:
+        self.value = value 
+        self.right = right 
+        self.left = left 
+
+    def show(self):
+        if self.right == None:
+            right = ' . '
+        else: 
+            right =  self.right.show()
+
+        if self.left == None:
+            left = ' . '
+        else: 
+            left =  self.left.show()
+        return "( " + left + str(self.value) + right + ")"
+
+
+# c = line(a,b)
+# distance = c.length()
+# print(distance)
+
+a = binarytree(3,None,None)
+b = binarytree(4, a, None)
+print(a.show())
+print(b.show())
